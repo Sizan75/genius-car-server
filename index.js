6107b13322766b759c0b3ccd1653b88848c4f352
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-03 02:52:15 +0600

    1 prob solved

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app= express()
 const port= process.env.PORT || 5000;
 
 app.use(express.json())
-app.cors(cors())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('genius car server running')
