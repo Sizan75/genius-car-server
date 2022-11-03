068fa3b21d02c4c26997ebb84442578429cfd621
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-03 10:12:44 +0600

    env veriable added for hide user info

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express=require('express')
 const cors=require('cors')
 const app= express()
 const port= process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+require('dotenv').config()
 app.use(express.json())
 app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('genius car server running')
